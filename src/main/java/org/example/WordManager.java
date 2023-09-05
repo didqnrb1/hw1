cc7b88fcc1a7da824ef98c129538b9366a539072
didqnrb1 <didqnrb1@handong.ac.kr>
2023-09-05 21:29:53 +0900

    lms에 올라온 두번째 강의 다 듣고 모든 기능 다 구현했습니다.

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -5,39 +5,61 @@ public class WordManager {
 
     Scanner s = new Scanner(System.in);
     WordCRUD wordCRUD;
-    WordManager(){wordCRUD = new WordCRUD(s);
+
+    WordManager() {
+        wordCRUD = new WordCRUD(s);
     }
+
     public int selectMenu() {
-            System.out.println(
-            "********************\n" +
-            "1. 모든 단어 보기\n" +
-            "2. 수준별 단어 보기\n" +
-            "3. 단어 검색\n" +
-            "4. 단어 추가\n" +
-            "5. 단어 수정\n" +
-            "6. 단어 삭제\n" +
-            "7. 파일 저장\n" +
-            "0. 나가기\n" +
-            "******************** \n" );
+        System.out.println(
+                "********************\n" +
+                        "1. 모든 단어 보기\n" +
+                        "2. 수준별 단어 보기\n" +
+                        "3. 단어 검색\n" +
+                        "4. 단어 추가\n" +
+                        "5. 단어 수정\n" +
+                        "6. 단어 삭제\n" +
+                        "7. 파일 저장\n" +
+                        "0. 나가기\n" +
+                        "******************** \n");
 
-            System.out.print("=> 원하는 메뉴는 ? ");
+        System.out.print("=> 원하는 메뉴는 ? ");
 
-            return s.nextInt();}
+        return s.nextInt();
+    }
 
-    public void start(){
+    public void start() {
+        wordCRUD.loadFile();
+
         System.out.println("*** 영단어 마스터 ***\n");
 
-            while(true){
-                int menu = selectMenu();
+        while (true) {
+            int menu = selectMenu();
 
-                    if(menu == 0) {
-                    System.out.println("\n프로그램 종료! 다음에 만나요~");
-                    break;}
-                    else if(menu == 4) wordCRUD.addWord();
+            if (menu == 0) {
+                System.out.println("\n프로그램 종료! 다음에 만나요~");
+                break;
+            } else if (menu == 4) wordCRUD.addItem();
 
-                    else if(menu == 1) wordCRUD.listAll();
-                        }
-                }
+            else if (menu == 1) wordCRUD.listAll();
+
+            else if (menu == 2) wordCRUD.searchLevel();
+
+            else if (menu == 3) wordCRUD.searchWord();
+
+            else if (menu == 5) {
+                //update
+                wordCRUD.updateItem();
+            } else if (menu == 6) {
+                wordCRUD.deleteItem();
+            }
+             else if (menu == 7) {
+            wordCRUD.saveFile();}
+
+        }
+
+
+    }
 
 }
 
